afeeb92c960cda3976fe5780049ee58edf9e24d4
Diego Casati <diego.casati@microsoft.com>
2025-10-28 19:15:21 +0000

    feat: Restore custom domain azureglobalblackbelts.com with correct configuration

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://appdevgbb.github.io',
+  url: 'https://azureglobalblackbelts.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/gbb-blog/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
