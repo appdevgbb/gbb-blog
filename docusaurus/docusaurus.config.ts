7faba0b97a81465ac7476830e2e15b118fdcf2af
Diego Casati <diego.casati@microsoft.com>
2025-10-28 19:01:43 +0000

    fix: Revert to GitHub Pages default URL (appdevgbb.github.io/gbb-blog)

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://azureglobalblackbelts.com',
+  url: 'https://appdevgbb.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/gbb-blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
